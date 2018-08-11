e6af41307d5f86c86b5e10701a18b989076bcd67
Natalie <natalie.todd15@gmail.com>
2018-08-11 15:50:36 -0600

    added hello galvanize

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
+    <h1>{{msg}}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      msg: 'Hello Galvanize',
+    };
+  },
 };
 </script>
 
