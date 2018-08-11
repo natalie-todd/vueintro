3f68b8d9c63dc32e3e4ecc20ef3672bdb3e12da1
Natalie <natalie.todd15@gmail.com>
2018-08-11 16:54:47 -0600

    added searchterm

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,17 +1,28 @@
 <template lang="html">
-<div>
-<h1>Movies!</h1>
-<form></form>
-<label for="search">Search</label>
-<input type="text" name="search" id="search" value="">
-<button type="submit" name="button">Search for Movies!</button>
-</div>
+    <div>
+        <h1>Movies!</h1>
+        <form @submit="searchForMovies()">
+            <label for="search">Search</label>
+            <input v-model="searchTerm" type="text" name="search" id="search" value="">
+            <button type="submit" name="button">Search for Movies!</button>
+        </form>
+        <h3>{{searchTerm}}</h3>
+        </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      searchTerm: ""
+    };
+  },
+  methods: {
+    searchForMovies() {
+      console.log("Searching for movies.");
+    }
+  }
+};
 </script>
 
 <style lang="css">
