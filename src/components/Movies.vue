98776ff0c4a07b09997f1a8564bdafe6712caf23
Natalie <natalie.todd15@gmail.com>
2018-08-11 16:45:50 -0600

    added form

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,5 +1,11 @@
 <template lang="html">
-
+<div>
+<h1>Movies!</h1>
+<form></form>
+<label for="search">Search</label>
+<input type="text" name="search" id="search" value="">
+<button type="submit" name="button">Search for Movies!</button>
+</div>
 </template>
 
 <script>
