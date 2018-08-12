9d164ba42c5f41753a0ed1733692dbc0d1482090
Natalie <natalie.todd15@gmail.com>
2018-08-12 11:28:53 -0600

    trying to get list to render, isn't working but bullet points are showing

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,10 +1,31 @@
 <template lang='html'>
-    
+    <div class=''>
+        <form @submit.prevent='addTodo()'>
+            <label for='newTodo'>New Todo</label>
+            <input type='text' name='newTodo' id='newTodo' value=''>
+            <button type='submit' name='button'>Add Todo</button>
+            </form>
+            <ul>
+              <li v-for='todo in todos'>{{todo}}</li>
+            </ul>
+        </div>
 </template>
 
 <script>
 export default {
-}
+  data() {
+    return {
+      newTodo: '',
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push(this.newTodo);
+      this.newTodo = '';
+    },
+  },
+};
 </script>
 
 <style lang='css'>
